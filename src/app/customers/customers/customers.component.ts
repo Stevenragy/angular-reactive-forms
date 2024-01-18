8bdda7f32dce4936ce5ebdd8f40466b267359ec6
Steven Ragy <stevenragy@github.com>
2024-01-18 16:06:23 +0200

    lesson 4: using reactive form and some reactive functions like setValue, patchValue and also using form builder

--- src/app/customers/customers/customers.component.ts
+++ src/app/customers/customers/customers.component.ts
@@ -1,24 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule, OnInit } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { Customer } from './customers';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
 export class CustomerComponent implements OnInit {
+  customerForm!: FormGroup;
   customer = new Customer();
+  private formBuilder = inject(FormBuilder);
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customerForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true,
+    });
+  }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      secondCatalog: false,
+    });
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
